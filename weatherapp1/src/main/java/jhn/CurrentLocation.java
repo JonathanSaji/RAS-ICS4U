9ecd0fc0f00ae2f4e9042b030b9f403fcb70c717
Jonathan Saji <jonathansaji2009@gmail.com>
2026-02-13 13:02:45 -0500

    get current lat and lon

---
+++ weatherapp1/src/main/java/jhn/CurrentLocation.java
@@ -0,0 +1,66 @@
+package jhn;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class CurrentLocation {
+
+    JSONObject dataObject;
+
+    public CurrentLocation() {
+        try {
+
+            URL url = new URL("http://ip-api.com/json/" + new GetIP().getIP());
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            // check if the connection is made (debugging)
+            int responseCode = conn.getResponseCode();
+
+            // 200 = ok
+            if (responseCode != 200) {
+                // throw an error
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+
+            } else {
+
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()) {
+                    // just puts everything in one big line
+                    informationString.append(scanner.nextLine());
+                }
+                // close the scanner
+                scanner.close();
+
+                // Change 1: Parse as JSONObject, not JSONArray
+                JSONParser parse = new JSONParser();
+                dataObject = (JSONObject) parse.parse(String.valueOf(informationString));
+
+                System.out.println("Lat: " + getLat());
+                System.out.println("Lon: " + getLon());
+            }
+            // not the best way to catch but it works!
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public double getLat() {
+        return (double) dataObject.get("lat");
+    }
+
+    public double getLon() {
+        return (double) dataObject.get("lon");
+    }
+
+    public static void main(String[] args) {
+        new CurrentLocation();
+    }
+}
